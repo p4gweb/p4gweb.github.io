43ea1adfe305ea249cdd1d1cb11c76e0b981a47e
Jim Tittsler <jimt@onjapan.net>
2020-10-15 20:33:39 +0900

    Use project name for page titles

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,9 +1,5 @@
-<script>
-  export let title;
-</script>
-
 <svelte:head>
-    <title>{title ? `${title} - Phase 4 Ground` : 'Phase 4 Ground'}</title>
+    <title>Phase 4 Ground</title>
 </svelte:head>
 
 <slot></slot>
